cf42ea2208089493f31e9a938ab1368e2749d722
Vaibhav Pandey <vaibhavpandey2121@gmail.com>
2026-05-05 17:32:25 +0530

    feat: add dynamic react object

--- Day1/tests.js
+++ Day1/tests.js
@@ -1,19 +1,34 @@
 //create element through js
-const header1=document.createElement('h1');
-header1.textContent="Hello Vaibhav";
 
-header1.style.backgroundColor="blue";
-header1.style.fontSize="30px";
-header1.style.color="white";
 
-//redundant
-const header2=document.createElement('h1');
-header2.textContent="Whats up";
+const React={
+    createElement: (tag,property,children)=>{
+        const element=document.createElement(tag);
+        for(let [key,val] of Object.entries(property)){
+            element.style[key]=val;
+        }
+        element.textContent=children;
+        return element;
+    }
+}
 
-header2.style.backgroundColor="black";
-header2.style.fontSize="25px";
-header2.style.color="white";
+// const header1=document.createElement('h1');
+// header1.textContent="Hello Vaibhav";
+
+// header1.style.backgroundColor="blue";
+// header1.style.fontSize="30px";
+// header1.style.color="white";
+
+// //redundant
+// const header2=document.createElement('h1');
+// header2.textContent="Whats up";
+
+// header2.style.backgroundColor="black";
+// header2.style.fontSize="25px";
+// header2.style.color="white";
+
+const header1=React.createElement('h1',{"backgroundColor":"black","color":"white"},"Hello Vaibhav");
 
 const div=document.getElementById('root');
 div.append(header1);
-div.append(header2);
+// div.append(header2);
