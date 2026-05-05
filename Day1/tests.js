cebab8f7524206e39d655866c26e77339a707779
Vaibhav Pandey <vaibhavpandey2121@gmail.com>
2026-05-05 18:53:36 +0530

    feat: improve react obj functionality

--- Day1/tests.js
+++ Day1/tests.js
@@ -1,17 +1,27 @@
 //create element through js
 
-
+//REACT OBJECT
 const React={
     createElement: (tag,property,children)=>{
         const element=document.createElement(tag);
         for(let [key,val] of Object.entries(property)){
             element.style[key]=val;
         }
-        element.textContent=children;
+
+        if(tag==='ul'){
+            for(let val of children){
+                const li = document.createElement("li");
+                li.textContent = val;
+                element.append(li);
+            }
+        }else{
+            element.textContent=children;
+        }
         return element;
     }
 }
 
+//REACTDOM OBJECT
 const ReactDOM={
     render: function(element,root){
         root.append(element);
@@ -35,9 +45,11 @@ const ReactDOM={
 
 const header1=React.createElement('h1',{"backgroundColor":"black","color":"white"},"Hello Vaibhav");
 const header2=React.createElement('h1',{"backgroundColor":"pink","color":"red"},"Hello Vaibhav");
+const ul=React.createElement('ul',{"backgroundColor":"pink","color":"red"},["mango","apple"]);
 
 const div=document.getElementById('root');
 ReactDOM.render(header1,div);
 ReactDOM.render(header2,div);
+ReactDOM.render(ul,div);
 // div.append(header1);
 // div.append(header2);
