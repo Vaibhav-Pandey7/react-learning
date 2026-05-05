041e98d5953fb16dcc559d2f6bffcdfdd827f66f
Vaibhav Pandey <vaibhavpandey2121@gmail.com>
2026-05-06 01:34:48 +0530

    chore: Initial setup

---
+++ Day2/app.js
@@ -0,0 +1,6 @@
+const element=React.createElement("h1",{id:"first",className:"vaibhav"},"i am vaibhav pandey");
+
+const div=document.getElementById('root');
+
+const root=ReactDOM.createRoot(div);
+root.render(element);
