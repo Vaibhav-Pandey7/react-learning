57421ac1b4c0c4fb86c6be92b698fb56997c1c06
Vaibhav Pandey <vaibhavpandey2121@gmail.com>
2026-05-06 13:36:56 +0530

    feat: implement vite

--- Day2/app.jsx
+++ Day2/app.jsx
@@ -1,12 +1,16 @@
+import React from "react";
+import {createRoot} from "react-dom/client"
 // const element=React.createElement("h1",{id:"first",className:"vaibhav",style:{backgroundColor:"pink",fontSize:"30px"}},"i am vaibhav pandey");
 // const element2=React.createElement("h1",{id:"second",className:"vaibhav",style:{backgroundColor:"red",fontSize:"30px"}},"content");
-const element=<h1 id="first" className="vaibhav" style={{ backgroundColor: "pink", fontSize: "30px" }}>i am vaibhav pandey</h1>
-const element2 = <h1 id="second" className="vaibhav" style={{ backgroundColor: "red", fontSize: "30px" }}>content</h1>
-
 const div=document.getElementById('root');
-const div1=React.createElement('div',{},[element,element2]);
+const div1=(
+    <div>
+        <h1 id="first" className="vaibhav" style={{ backgroundColor: "pink", fontSize: "30px" }}>i am vaibhav pandey</h1>
+        <h1 id="second" className="vaibhav" style={{ backgroundColor: "red", fontSize: "30px" }}>content</h1>
+    </div>
+);
 
-const root=ReactDOM.createRoot(div);
-root.render(element);
-root.render(element2);  
-root.render(div1);
+createRoot(div).render(div1);
+// root.render(element);
+// root.render(element2);  
+// root.render(div1)
--- Day2/readme.md
+++ Day2/readme.md
@@ -4,3 +4,4 @@ Bundler->used to make the code production ready
 Babel->used to convert JSX->React compatible code
 but here we are usign vite that has its inbuilt esbuild that does the work of babel for us
 
+npm->registry
