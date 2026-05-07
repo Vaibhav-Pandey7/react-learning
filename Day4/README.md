d3fe1c4bb3525ea6e205dff12b06095b16522081
Vaibhav Pandey <vaibhavpandey2121@gmail.com>
2026-05-08 01:24:57 +0530

    chore: practice props

--- Day4/README.md
+++ Day4/README.md
@@ -1,3 +1,4 @@
 we only write js expressions inside jsx   
 expressions basically means any thing that produces a result  
-we dont write statements(like let x=10;) 
+we dont write statements(like let x=10;)   
+React components are just JS functions, JSX is just a translator, and props(properties) are just automatically bundled object arguments.
--- Day4/src/main.jsx
+++ Day4/src/main.jsx
@@ -7,6 +7,11 @@ const array=[20,40,"Rohit",10];
 function greet() {
   return <h2>Hello Bhai ji {array}</h2>;
 }
+function Greet(props) {
+  return <h2>Hello Bhai ji {props.name}</h2>;
+}
 //we can suse array in jsx 
 
-createRoot(document.getElementById('root')).render(greet());
+// this is jsx rule that first letter is capital no react
+
+createRoot(document.getElementById('root')).render(<Greet name="Vaibhav Pandey" age="20"/>);
