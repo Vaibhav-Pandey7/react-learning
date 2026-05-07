82140ffb4a90aa2cf259a7f5e14a02ce0d9b3dda
Vaibhav Pandey <vaibhavpandey2121@gmail.com>
2026-05-08 00:18:47 +0530

    chore: understand {}operator in jsx

---
+++ Day4/README.md
@@ -0,0 +1,3 @@
+we only write js expressions inside jsx   
+expressions basically means any thing that produces a result  
+we dont write statements(like let x=10;) 
--- Day4/src/main.jsx
+++ Day4/src/main.jsx
@@ -2,8 +2,11 @@ import { createRoot } from 'react-dom/client'
 
 const element1=<h1>Hello Everyone</h1>
 
-function greet() {
-  return <h2>Hello Bhai ji</h2>;
-}
+const array=[20,40,"Rohit",10];
 
-createRoot(document.getElementById('root')).render(element1);
+function greet() {
+  return <h2>Hello Bhai ji {array}</h2>;
+}
+//we can suse array in jsx 
+
+createRoot(document.getElementById('root')).render(greet());
